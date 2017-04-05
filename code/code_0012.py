da610eb7b49344f49ffe427d318770ccf629e35f
zhiyongwu <zhiyongwu@live.cn>
2017-04-05 17:38:11 +0800

    commit 12

--- code/code_0012.py
+++ code/code_0012.py
@@ -1,3 +1,17 @@
 """
 敏感词文本文件 filtered_words.txt，里面的内容 和 0011题一样，当用户输入敏感词语，则用 星号 * 替换，例如当用户输入「北京是个好城市」，则变成「**是个好城市」。
 """
+filtered_words = [word.strip() for word in open('../text/filtered_words.txt', encoding='utf-8')]
+
+
+def filter_word(word):
+    for each in filtered_words:
+        if each in word:
+            word = word.replace(each, '*' * len(each))
+    return word
+
+
+if __name__ == '__main__':
+    while True:
+        c_in = input()
+        print(filter_word(c_in))
